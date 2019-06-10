0fc5f13565f2793e143532191bc856ae1f40437a
Javafuel <Javafuel@github.com>
2019-06-10 14:27:41 +0000

    Add account activation without 11.4 - Email in Production

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,7 +16,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
   
-    test "login with valid information followed by logout" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
@@ -38,12 +38,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
   
-   test "login with remembering" do
+  test "login with remembering" do
     log_in_as(@user, remember_me: '1')
     assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
   
-   test "login without remembering" do
+  test "login without remembering" do
     # Log in to set the cookie.
     log_in_as(@user, remember_me: '1')
     # Log in again and verify that the cookie is deleted.
